9bab272ec727ff7d10aa07cd5baf0497e451fc4c
AyushSatyam <42187810+AyushSatyam@users.noreply.github.com>
2019-04-12 20:45:22 +0530

    phase1
    A payment gateway intrigration in a web page using payUmoney payment gateway.

---
+++ Home/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="favicon.ico">
+		<title>Home</title>
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/5.6.3/css/font-awesome.min.css">
+		
+		<!-- Custom styles for this template -->
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<link href="css/owl.theme.default.min.css"  rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/price.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+		<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+		<script src="js/ie-emulation-modes-warning.js"></script>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body id="page-top">
+		<!-- Navigation -->
+			<nav class="navbar navbar-expand-lg navbar-fixed-top" id="navfull">
+					<a class="navbar-brand" href="#"><p id="logo">SPARK</p></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+			  </nav>
+		<!-- Header -->
+		<header>
+			<div class="container text-left">
+				<div class="slider-container">
+					<div class="intro-text">
+						<div class="intro-heading">A New Way</div>
+						<div class="intro-heading">To Start Business</div><br><br>
+						<h7>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h7>  <br>
+						<h7>	eaque error nemo ducimus hic, quod pariatur doloremque.</h7><br>
+						<div class='btn pm-button'><a href='https://www.payumoney.com/paybypayumoney/#/4090F760ABD1F3ABB1F5B9EE204C431B' style="Decoration:none" ><button class="btn btn-success" style="margin-top:0px;
+						margin-left:0px;
+						padding: 10px 40px;">Donate</button></a></div>
+						
+
+					</div>
+				</div>
+			</div>
+		</header>
+		<section id="about" class="light-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>Smartest protection for your site</h2>
+						</div>
+					</div>
+				</div><br><br>
+				<div class="row">
+					<!-- about module -->
+					<div class="col-md-4 text-center">
+						<div class="mz-module-about">
+							<img src="spam.jpg" alt="Anti-spam" height="60px" width="60px">
+							<h3>Great Internship</h3>
+							<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+						</div>
+					</div>
+					<!-- end about module -->
+					<!-- about module -->
+					<div class="col-md-4 text-center">
+						<div class="mz-module-about">
+							<img src="phishing.jpg" alt="phishing" height="60px" width="60px">
+							<h3>Great Oportunity</h3>
+							<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+						</div>
+					</div>
+					<!-- end about module -->
+					<!-- about module -->
+					<div class="col-md-4 text-center">
+						<div class="mz-module-about">
+							<img src="scan.jpg" alt="scan" height="60px" width="60px">
+							<h3>Full Access</h3>
+							<p>Accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+						</div>
+					</div>
+					<!-- end about module -->
+					<!-- end about module -->
+				</div></section><br><br>
+				
+		<section id="contact">
+				<div class="container">
+						<div class="row">
+								<div class="col-md-6">
+										<h2 style="color:white;
+										font-weight: bold;">Contact Us</h2>
+										<p>If you have some Questions or need Help! Please Contact Us!<br>We make Cool and Clean Design for your Business</p>
+
+									<p style="color:#fff;">Email:company_email.com</p>
+									<p style="color:#fff;">Phone:361-688-5824</p>
+									<p style="color:#fff;">Address:4826 White Avenue,Corpus Kerala,India</p>
+								</div>
+								<div class="col-md-6">
+									<form name="sentMessage" id="contactForm" novalidate="">
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<label style="color: rgb(4, 136, 37);" >Your Name</label>
+													<input type="text" class="form-control" id="name" required="" data-validation-required-message="Please enter your name.">
+													<p class="help-block text-danger"></p>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group">
+														<label style="color: rgb(4, 136, 37);">Your Email</label>
+													<input type="email" class="form-control" id="email" required="" data-validation-required-message="Please enter your email address.">
+													<p class="help-block text-danger"></p>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<div class="form-group">
+														<label style="color: rgb(4, 136, 37);">Message</label>
+													<textarea class="form-control" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
+													<p class="help-block text-danger"></p>
+												</div>
+											</div>
+											<div class="clearfix"></div>
+										</div>
+										<div class="row">
+											<div class="col-lg-12 text-center">
+												<div id="success"></div>
+												<button type="submit" class="btn btn-lg btn-block" style="background-color: #087c39; color: rgb(15, 15, 48)" >Submit</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+				</div>
+		</section>
+		<footer>
+			<div class="container-fluid">
+					<div class="col-md-4 text-left">
+							<p>Copyright &copy; 2018 Spark.</a></p>
+						</div>
+						<div class="col-md-5"></div>
+						<div class="col-md-3 row text-left" id="social">
+								<ul class="social-network social-circle ">
+									<li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+									<li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+								</ul>				
+							</div>
+			</div>
+		</footer>
+
+
+		<!-- Bootstrap core JavaScript
+			================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/cbpAnimatedHeader.js"></script>
+		<script src="js/theme-scripts.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="js/ie10-viewport-bug-workaround.js"></script>
+		<!--jquery-->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+	</body>
+</html>
